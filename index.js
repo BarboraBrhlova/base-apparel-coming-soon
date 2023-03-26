29e5dfdec2f3a0fa3f984b77c1a676f53adcff37
BarboraB <barbora.brhlova@seznam.cz>
2023-03-26 15:40:36 +0200

    start project

---
+++ index.js
@@ -0,0 +1,19 @@
+document.querySelector(".button").addEventListener("click", function () {
+  var isEmailAddressValid = validateEmailAddress();
+  if (isEmailAddressValid) {
+    document.querySelector(".invalidEmail").style.visibility = "hidden";
+    document.querySelector(".errorIcon").style.visibility = "hidden";
+  } else {
+    document.querySelector(".invalidEmail").style.visibility = "visible";
+    document.querySelector(".errorIcon").style.visibility = "visible";
+  }
+});
+
+function validateEmailAddress() {
+  var emailAddress = document.querySelector(".form-text").value;
+  if (emailAddress.includes("@") && emailAddress.includes(".")) {
+    return true;
+  } else {
+    return false;
+  }
+}
